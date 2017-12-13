c13e38927c734fa755c77ff270dc6ee44cab5930
pettingbror <peter.brymer@hotmail.com>
2017-12-13 17:24:22 +0100

    First update with Character and Tank classes.

--- mmoUppgift/src/CharacterPack/Character.java
+++ mmoUppgift/src/CharacterPack/Character.java
@@ -1,4 +1,33 @@
 package CharacterPack;
 
+import ItemPack.Items;
+
+import java.util.ArrayList;
+
 public class Character {
+    private String name;
+    private String gender;
+    private int[];
+    private ArrayList<Items> bag;
+    private ArrayList<Items> equipedItems;
+    private ArrayList<Character> friendList;
+
+    public Character(String name, String gender,int classes) {
+        this.name = name;
+        this.gender = gender;
+        this.bag = new ArrayList<>();
+        this.equipedItems = new ArrayList<>();
+        this.friendList = new ArrayList<>();
+        if(classes ==0 ){
+            Tank tank = new Tank();
+        }
+        if(classes ==1){
+            Warrior warrior = new Warrior();
+        }
+        if(classes ==2){
+            Wizard wizard = new Wizard();
+        }
+    }
+
+
 }
--- mmoUppgift/src/CharacterPack/Tank.java
+++ mmoUppgift/src/CharacterPack/Tank.java
@@ -1,4 +1,39 @@
 package CharacterPack;
 
 public class Tank {
+    private int basePower;
+    private int baseArmor;
+    private int baseMana;
+    private int baseHp;
+
+    public Tank() {
+        this.basePower = 25;
+        this.baseArmor = 70;
+        this.baseMana = 15;
+        this.baseHp = 150;
+    }
+
+    public int shieldBlock(int armor){
+        return armor *2;
+    }
+
+    public int shieldBash(int armor, int power){
+        return armor+power;
+    }
+
+    public int getBasePower() {
+        return basePower;
+    }
+
+    public int getBaseArmor() {
+        return baseArmor;
+    }
+
+    public int getBaseMana() {
+        return baseMana;
+    }
+
+    public int getBaseHp() {
+        return baseHp;
+    }
 }

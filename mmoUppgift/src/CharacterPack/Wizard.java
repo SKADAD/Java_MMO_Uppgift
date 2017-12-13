780a4b05dfad52709f88ba0460348c84e563fc95
Simonn89 <32932285+Simonn89@users.noreply.github.com>
2017-12-13 17:12:20 +0100

    Wizard

--- mmoUppgift/src/CharacterPack/Wizard.java
+++ mmoUppgift/src/CharacterPack/Wizard.java
@@ -1,4 +1,39 @@
 package CharacterPack;
 
 public class Wizard {
+    private int basePower;
+    private int baseArmor;
+    private int baseMana;
+    private int baseHp;
+
+    public Wizard(int basePower, int baseArmor, int baseMana, int baseHp) {
+        this.basePower = 10;
+        this.baseArmor = 10;
+        this.baseMana = 50;
+        this.baseHp = 80;
+    }
+
+    public int getBasePower() {
+        return basePower;
+    }
+
+    public int getBaseArmor() {
+        return baseArmor;
+    }
+
+    public int getBaseMana() {
+        return baseMana;
+    }
+
+    public int getBaseHp() {
+        return baseHp;
+    }
+
+    public int LightningBold(int Power){
+        return Power*3;
+    }
+
+    public int Heal(int HP){
+        return HP;
+    }
 }

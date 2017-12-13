6f3bb2a76df5b0c725930dc0cd7e644248b29a27
Simonn89 <32932285+Simonn89@users.noreply.github.com>
2017-12-13 17:51:11 +0100

    Wizard fix

--- mmoUppgift/src/User.java
+++ mmoUppgift/src/User.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+
 public class User {
+    private String Username;
+    private String Password;
+    private String email;
+    private ArrayList<Character> ListofCharacters;
+
+
+    public User(String Username, String Password, String email){
+        this.Username = Username;
+        this.Password = Password;
+        this.email = email;
+        this.ListofCharacters = new ArrayList<>();
+    }
+
+
+
 }

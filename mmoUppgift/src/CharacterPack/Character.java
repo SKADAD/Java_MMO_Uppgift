bc1ba57e5c221ecac13dfc190dc782fffb9e3a7a
pettingbror <peter.brymer@hotmail.com>
2017-12-13 17:43:52 +0100

    Update på character

--- mmoUppgift/src/CharacterPack/Character.java
+++ mmoUppgift/src/CharacterPack/Character.java
@@ -10,7 +10,11 @@ import java.util.ArrayList;
 public class Character {
     private String name;
     private String gender;
-    private int[];
+    private String fightclass;
+    private int armor;
+    private int hp;
+    private int mana;
+    private int power;
     private ArrayList<Items> bag;
     private ArrayList<Items> equipedItems;
     private ArrayList<Character> friendList;
@@ -23,14 +27,67 @@ public class Character {
         this.friendList = new ArrayList<>();
         if(classes ==0 ){
             Tank tank = new Tank();
+            armor =tank.getBaseArmor();
+            hp =tank.getBaseHp();
+            mana =tank.getBaseMana();
+            power=tank.getBasePower();
+            fightclass = "Tank";
         }
         if(classes ==1){
             Warrior warrior = new Warrior();
+            armor =warrior.getBaseArmor();
+            hp =warrior.getBaseHp();
+            mana =warrior.getBaseMana();
+            power =warrior.getBasePower();
+            fightclass ="Warrior";
         }
         if(classes ==2){
             Wizard wizard = new Wizard();
+            armor = wizard.getBaseArmor();
+            hp = wizard.getBaseHp();
+            mana = wizard.getBaseMana();
+            power = wizard.getBasePower();
+            fightclass = "Wizard";
         }
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public String getGender() {
+        return gender;
+    }
+
+    public String getFightclass() {
+        return fightclass;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getMana() {
+        return mana;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public ArrayList<Items> getBag() {
+        return bag;
+    }
+
+    public ArrayList<Items> getEquipedItems() {
+        return equipedItems;
+    }
+
+    public ArrayList<Character> getFriendList() {
+        return friendList;
+    }
 }

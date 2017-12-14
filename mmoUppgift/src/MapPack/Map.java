9e811bed13be0b7c9dcb40514c58bc8b84836474
SKADAD <32932385+SKADAD@users.noreply.github.com>
2017-12-14 16:46:44 +0100

    Merge branch 'master' of https://github.com/SKADAD/Java_MMO_Uppgift
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.
    
    la till typ 5 rader i map av skräp.

--- mmoUppgift/src/MapPack/Map.java
+++ mmoUppgift/src/MapPack/Map.java
@@ -1,4 +1,14 @@
 package MapPack;
 
 public class Map {
+
+    private int xCordinateBase;
+    private int yCordinateBase;
+
+    public Map(int xCordinateBase, int yCordinateBase) {
+        this.xCordinateBase = xCordinateBase;
+        this.yCordinateBase = yCordinateBase;
+
+    }
+
 }

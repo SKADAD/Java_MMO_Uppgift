571245cdd6a57bed766844d0f4cfb2d898cbf104
SKADAD <32932385+SKADAD@users.noreply.github.com>
2017-12-18 14:31:34 +0100

    Added stuff to playarea, still issues on row 19

--- mmoUppgift/src/MapPack/PlayArea.java
+++ mmoUppgift/src/MapPack/PlayArea.java
@@ -1,26 +1,22 @@
 package MapPack;
 
+import java.util.Random;
+
 public class PlayArea extends Map{
 
-
-    public PlayArea(){
+    public PlayArea(int xCordinateBase,int yCordinateBase){
+        super(xCordinateBase, yCordinateBase);
 
         String[][] map = new String[xCordinateBase][yCordinateBase];
 
-        int forLoopNum = (((xCordinateBase+yCordinateBase)/2)*0.1);
+        int forLoopNum = (((xCordinateBase+yCordinateBase)/2)/10;
 
 
         for(int i = 0;forLoopNum >= i; i++ ){
             Random num = new Random();
             int randNum = num.nextInt((xCordinateBase+yCordinateBase)/2);
             int randNum1 = num.nextInt((xCordinateBase+yCordinateBase)/2);
-            String[randNum][randNum1] = 1;
+            map[randNum][randNum1].set("1");
         }
-
-
-
-
     }
-
-
 }

6cbae9fff577b5af2ebe66d30c5b5d247e26ac54
SKADAD <32932385+SKADAD@users.noreply.github.com>
2017-12-18 11:12:04 +0100

    Added vendor

--- mmoUppgift/src/VendorPack/MainVendor.java
+++ mmoUppgift/src/VendorPack/MainVendor.java
@@ -1,4 +1,17 @@
 package VendorPack;
 
 public class MainVendor {
+    private ArrayList<charitems> sellableItems;
+
+    public MainVendor(ArrayList<charitems> sellableItems) {
+        this.sellableItems = new ArrayList<>();
     }
+
+    public ArrayList<charitems> getSellableItems() {
+        return sellableItems;
+    }
+
+    public void setSellableItems(ArrayList<charitems> sellableItems) {
+        this.sellableItems = sellableItems;
+    }
+}

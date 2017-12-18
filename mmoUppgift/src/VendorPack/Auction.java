90ee52b7de3221684bee7e1def9293c6cf48139b
Simonn89 <32932285+Simonn89@users.noreply.github.com>
2017-12-18 10:52:53 +0100

    Fixed a class

--- mmoUppgift/src/VendorPack/Auction.java
+++ mmoUppgift/src/VendorPack/Auction.java
@@ -1,4 +1,33 @@
 package VendorPack;
 
+import CharacterPack.character;
+import ItemPack.charItems;
+
+import java.util.ArrayList;
+
 public class Auction {
+    private ArrayList<charItems> auctionsList;
+    private ArrayList<character> sellerList;
+
+    public Auction(ArrayList<charItems> auctionsList, ArrayList<character> sellerList) {
+        this.auctionsList = new ArrayList<>();
+        this.sellerList = new ArrayList<>();
+    }
+
+    public ArrayList<charItems> getAuctionsList() {
+        return auctionsList;
+    }
+
+    public void setAuctionsList(ArrayList<charItems> auctionsList) {
+        this.auctionsList = auctionsList;
+    }
+
+    public ArrayList<character> getSellerList() {
+        return sellerList;
+    }
+
+    public void setSellerList(ArrayList<character> sellerList) {
+        this.sellerList = sellerList;
+    }
 }
+
--- mmoUppgift/src/VendorPack/MainVendor.java
+++ mmoUppgift/src/VendorPack/MainVendor.java
@@ -1,4 +1,4 @@
 package VendorPack;
 
 public class MainVendor {
-}
+    }

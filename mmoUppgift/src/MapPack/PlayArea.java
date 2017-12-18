9e9b05397e673de004a7636552961c458066c826
SKADAD <32932385+SKADAD@users.noreply.github.com>
2017-12-18 10:28:19 +0100

    Merge branch 'master' of https://github.com/SKADAD/Java_MMO_Uppgift
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.
    
    Added playarea and generating immovable objects.

--- mmoUppgift/src/MapPack/PlayArea.java
+++ mmoUppgift/src/MapPack/PlayArea.java
@@ -1,4 +1,26 @@
 package MapPack;
 
-public class PlayArea {
+public class PlayArea extends Map{
+
+
+    public PlayArea(){
+
+        String[][] map = new String[xCordinateBase][yCordinateBase];
+
+        int forLoopNum = (((xCordinateBase+yCordinateBase)/2)*0.1);
+
+
+        for(int i = 0;forLoopNum >= i; i++ ){
+            Random num = new Random();
+            int randNum = num.nextInt((xCordinateBase+yCordinateBase)/2);
+            int randNum1 = num.nextInt((xCordinateBase+yCordinateBase)/2);
+            String[randNum][randNum1] = 1;
+        }
+
+
+
+
+    }
+
+
 }

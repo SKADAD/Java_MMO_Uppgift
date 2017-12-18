45538756022928ffb3de3b5031d59b662e4e7c31
Simonn89 <32932285+Simonn89@users.noreply.github.com>
2017-12-18 15:03:36 +0100

    Lagar nabbes kod

--- mmoUppgift/src/MapPack/PlayArea.java
+++ mmoUppgift/src/MapPack/PlayArea.java
@@ -1,5 +1,6 @@
 package MapPack;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class PlayArea extends Map{
@@ -8,17 +9,18 @@ public class PlayArea extends Map{
         super(xCordinateBase, yCordinateBase);
 
         String[][] map = new String[xCordinateBase][yCordinateBase];
+        ArrayList<ArrayList<Integer>> myDubbelArray = new ArrayList<>();
+
 
         int forLoopNum = (((xCordinateBase+yCordinateBase)/2)/10);
-
-
-        for(int i = 0;forLoopNum >= i; i++ ){
+        for(int i = 0;forLoopNum >= i; i++ ) {
             Random num = new Random();
-            int randNum = num.nextInt((xCordinateBase+yCordinateBase)/2);
-            int randNum1 = num.nextInt((xCordinateBase+yCordinateBase)/2);
-            if(i"null"){
-                map[randNum][randNum1].set("1");
-            }
+            int randNum = num.nextInt((xCordinateBase + yCordinateBase) / 2);
+            int randNum1 = num.nextInt((xCordinateBase + yCordinateBase) / 2);
+            map[randNum][randNum1].set("1");
+
+        }
+
     }
 }
 
